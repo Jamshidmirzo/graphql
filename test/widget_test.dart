f93e0ce9df4430c7ad5a68bbe7ed9dc7d1c53bc3
Jamshidmirzo <101593441+Jamshidmirzo@users.noreply.github.com>
2024-07-30 12:11:49 +0500

    improve

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:graphql/main.dart';
 import 'package:graphql/views/screens/home_screen.dart';
-
 
 void main() {
   testWidgets("My App Test", (tester) async {
-    await tester.pumpWidget(const HomeScreen());
+    await tester.pumpWidget(const Myapp());
     expect(find.text("Salom, CI/CD!"), findsOneWidget);
   });
 }
